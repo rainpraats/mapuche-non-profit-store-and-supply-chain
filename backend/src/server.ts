4a79c92e9e52220ade1c41edb1ea68f422f2ccdd
rainpraats <rainpraats@gmail.com>
2025-12-28 18:50:03 +0100

    raised api requests per hour since we hit the limit when doing testing.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,34 +14,33 @@ import { NODE_ENV, PORT } from './envVariables.js';
 const startServer = async () => {
   try {
     await connectDb();
-    
-    const app = express();
-    
-  app.use(helmet());
-  app.use(cors());
-  app.use('/api/', rateLimit({ max: 100, windowMs: 60 * 60 * 1000 }));
-  app.use(express.json({ limit: '1mb' }));
-  app.use(mongoSanitize());
-  app.use(xss());
-  app.use(hpp());
-  
-  if (NODE_ENV === 'development') {
-    app.use(logger);
-  }
 
-  app.use('/api/v1', router);
-  
-  app.use(errorHandler);
-  
-  app.listen(PORT, () =>
-    console.log(
-      `Backend running on http://localhost:${PORT} in ${NODE_ENV} mode.`
-    )
-  );
-} catch (error: any) {
-  throw new Error(error);
-}
-  
-}
+    const app = express();
+
+    app.use(helmet());
+    app.use(cors());
+    app.use('/api/', rateLimit({ max: 1000, windowMs: 60 * 60 * 1000 }));
+    app.use(express.json({ limit: '1mb' }));
+    app.use(mongoSanitize());
+    app.use(xss());
+    app.use(hpp());
+
+    if (NODE_ENV === 'development') {
+      app.use(logger);
+    }
+
+    app.use('/api/v1', router);
+
+    app.use(errorHandler);
+
+    app.listen(PORT, () =>
+      console.log(
+        `Backend running on http://localhost:${PORT} in ${NODE_ENV} mode.`
+      )
+    );
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
 
 startServer();
